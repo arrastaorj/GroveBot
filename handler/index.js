db2651ba2c8ba4856e704fd4fb56b5af66d83903
arrastaorj <grrodrigues1802@gmail.com>
2023-10-27 17:21:35 -0300

    42342

--- handler/index.js
+++ handler/index.js
@@ -77,6 +77,41 @@ module.exports = async (client) => {
       }
     }
 
+
+
+    app.post('/github', express.json({ type: 'application/json' }), (request, response) => {
+
+      response.status(202).send('Accepted');
+
+      const githubEvent = request.headers['x-github-event'];
+
+
+      if (githubEvent === 'issues') {
+        const data = request.body;
+        const action = data.action;
+        if (action === 'opened') {
+          console.log(`An issue was opened with this title: ${data.issue.title}`);
+        } else if (action === 'closed') {
+          console.log(`An issue was closed by ${data.issue.user.login}`);
+        } else {
+          console.log(`Unhandled action for the issue event: ${action}`);
+        }
+      } else if (githubEvent === 'ping') {
+        console.log('GitHub sent the ping event');
+      } else {
+        console.log(`Unhandled event: ${githubEvent}`);
+      }
+    });
+
+
+    const port = 3000;
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+
+
+
     // const channel = client.channels.cache.get(process.env.CHANNEL_ID)
 
     // if (channel) {
