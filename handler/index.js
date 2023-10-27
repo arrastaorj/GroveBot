126747cf660858a7c9670a3692017df2a5bd80a1
arrastaorj <grrodrigues1802@gmail.com>
2023-10-27 13:17:54 -0300

    terste

--- handler/index.js
+++ handler/index.js
@@ -4,6 +4,9 @@ const bot = require('../bot.json')
 const chalk = require('chalk')
 const axios = require('axios')
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
 module.exports = async (client) => {
 
   //Puxando os comandos em slash!
@@ -36,73 +39,46 @@ module.exports = async (client) => {
   })
 
 
-  const GITHUB_TOKEN = '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' // Substitua com seu token de acesso pessoal.
-  const GITHUB_REPO = 'arrastaorj/LexaV142023' // Substitua com o nome do proprietário e repositório GitHub.
-  const CHANNEL_ID = '1054128840642920468' // Substitua pelo ID do canal onde você deseja que os logs de commit sejam enviados.
 
-  client.once('ready', () => {
 
-    const channel = client.channels.cache.get(CHANNEL_ID);
 
-    if (channel) {
-      setInterval(async () => {
-        const latestCommit = await getLatestCommit(GITHUB_REPO);
+  const app = express();
+  const port = 3000; // Escolha a porta que você deseja usar
 
-        if (latestCommit) {
-          if (latestCommit !== lastCommitSent) {
-            lastCommitSent = latestCommit;
+  app.use(bodyParser.json());
 
-            const embed = new discord.EmbedBuilder()
-              .setColor('#ff0000')
-              .setTitle('**Novo Commit no Repositório**')
-              .addFields(
-                { name: 'Repositório', value: GITHUB_REPO },
-                { name: 'Hash do Commit', value: latestCommit.hash },
-                { name: 'Autor', value: latestCommit.author },
-                { name: 'Data do Commit', value: latestCommit.date },
-                { name: 'Mensagem do Commit', value: latestCommit.message }
-              )
-              .setURL(latestCommit.url)
-              .setTimestamp();
+  // Inicialize o bot e o canal aqui
 
-            channel.send({ embeds: [embed] });
-          }
-        }
-      }, 10000);
-    } else {
-      console.error(`Canal com ID ${CHANNEL_ID} não encontrado.`);
+  app.post('/github-webhook', (req, res) => {
+    const eventType = req.get('X-GitHub-Event');
+    if (eventType === 'push') {
+      const latestCommit = req.body.head_commit;
+      const embed = new discord.MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle('**Novo Commit no Repositório**')
+        .addFields(
+          { name: 'Repositório', value: process.env.GITHUB_REPO },
+          { name: 'Hash do Commit', value: latestCommit.id },
+          { name: 'Autor', value: latestCommit.author.name },
+          { name: 'Data do Commit', value: latestCommit.timestamp },
+          { name: 'Mensagem do Commit', value: latestCommit.message }
+        )
+        .setURL(latestCommit.url)
+        .setTimestamp();
+
+      // Envie a mensagem para o canal do Discord
+      const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+      if (channel) {
+        channel.send({ embeds: [embed] });
+      }
     }
+    res.sendStatus(200);
+  });
 
-  })
+  app.listen(port, () => {
+    console.log(`Servidor ouvindo na porta ${port}`);
+  });
 
-  let lastCommitSent = '';
-
-  async function getLatestCommit(repo) {
-    try {
-      const response = await axios.get(`https://api.github.com/repos/${repo}/commits`, {
-        headers: {
-          Authorization: `Bearer ${GITHUB_TOKEN}`,
-        },
-      });
-      const latestCommit = response.data[0];
-
-      const commitHash = latestCommit.sha;
-      const commitMessage = latestCommit.commit.message;
-      const commitAuthor = latestCommit.commit.author.name;
-      const commitDate = latestCommit.commit.author.date;
-
-      return {
-        hash: commitHash,
-        author: commitAuthor,
-        date: commitDate,
-        message: commitMessage,
-        url: latestCommit.html_url,
-      };
-    } catch (error) {
-      console.error('Erro ao buscar o último commit:', error);
-      return null;
-    }
-  }
 
 
 
