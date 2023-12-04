c3c7edb56c9f8d0a766efaf34c796adc34075011
arrastaorj <grrodrigues1802@gmail.com>
2023-12-04 20:13:08 -0300

    teste

--- index.js
+++ index.js
@@ -93,12 +93,12 @@ async function loadRiffy() {
 client.login(process.env.tokenGrove)
 
 
-// process.on('unhandRejection', (reason, promise) => {
-//   console.log(`❗ | [Erro]\n\n` + reason, promise)
-// })
-// process.on('uncaughtException', (error, origin) => {
-//   console.log(`❗ | [Erro]\n\n` + error, origin)
-// })
-// process.on('uncaughtExceptionMonitor', (error, origin) => {
-//   console.log(`❗ | [Erro]\n\n` + error, origin)
-// })
+process.on('unhandRejection', (reason, promise) => {
+  console.log(`❗ | [Erro]\n\n` + reason, promise)
+})
+process.on('uncaughtException', (error, origin) => {
+  console.log(`❗ | [Erro]\n\n` + error, origin)
+})
+process.on('uncaughtExceptionMonitor', (error, origin) => {
+  console.log(`❗ | [Erro]\n\n` + error, origin)
+})
