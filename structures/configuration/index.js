7e4ec6bbbcbcc33ed9d6fca2646d32b0051fe2a4
arrastaorj <grrodrigues1802@gmail.com>
2023-12-09 10:41:05 -0300

    teste

--- structures/configuration/index.js
+++ structures/configuration/index.js
@@ -4,11 +4,18 @@ module.exports = {
     developers: ["424244967893106699"],
     nodes: [
         {
-            host: "132.145.68.135",
-            port: 6016,
-            name: 'LavaLink 1',
-            password: "nagatoroLavaLink#@#@",
-            secure: false
+            host: "groveserver.discloud.app",
+            port: 443,
+            name: 'GroveServer',
+            password: "7856",
+            secure: true
         },
     ]
 }
+
+
+// host: "132.145.68.135",
+// port: 6016,
+// name: 'LavaLink 1',
+// password: "nagatoroLavaLink#@#@",
+// secure: false
