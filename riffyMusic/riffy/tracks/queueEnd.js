0e5fcf6b08ea1fed3a634a3488976fc5aef6c5c8
arrastaorj <grrodrigues1802@gmail.com>
2023-12-23 09:19:22 -0300

    teste

--- riffyMusic/riffy/tracks/queueEnd.js
+++ riffyMusic/riffy/tracks/queueEnd.js
@@ -1,23 +1,9 @@
 const client = require("../../../index")
 const { disconnectRow, disconnectRow2 } = require("../buttons/musicButtons")
 const { activeMessages } = require("./trackStart")
-const { AttachmentBuilder } = require("discord.js")
-const { musicCard } = require("musiccard")
 
 
 client.riffy.on("queueEnd", async (player) => {
-
-    const card = new musicCard()
-        .setName("Grove Music")
-        .setAuthor("Sofisticado")
-        .setColor("auto")
-        .setThumbnail("https://raw.githubusercontent.com/arrastaorj/flags/main/music.png")
-        .setProgress(0)
-        .setStartTime("00:00")
-        .setEndTime("00:00")
-
-    const buffer = await card.build()
-    const attachment = new AttachmentBuilder(buffer, { name: `musicard.png` })
 
     try {
 
@@ -35,9 +21,10 @@ client.riffy.on("queueEnd", async (player) => {
     } catch (error) {
         return
     }
-    
+
     if (player.isAutoplay) {
         player.autoplay(player)
+
     } else {
 
         const channel = client.channels.cache.get(player.textChannel)
